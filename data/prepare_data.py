8f0fb00ad7d59d32bc2d328b41e1844dbebb9b4d
Katarzyna P <84340033+hvkat@users.noreply.github.com>
2022-03-03 08:07:34 +0100

    Update prepare_data.py
    Changed back

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -27,7 +27,10 @@ def songsTensors(genres_directory, genres):
     for idx, g in enumerate(genres):
         path = genres_directory + g + '/'
         print(path)
-        songs_paths = [os.path.join(path, filename) for filename in os.listdir(path)]
+        songs_paths = []
+        for (dirpath, dirname, filenames) in os.walk(path):
+            for f in filenames:
+                songs_paths.append(os.path.join(path, f))
         for s_idx, s in enumerate(songs_paths):
             T = [j for j in range(26)]
             x, sr = librosa.load(s)
